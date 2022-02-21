493336244f25f49af044986a7ebaf4188ea81932
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2022-02-21 08:42:04 -0600

    Changed config.json "appId" to "app_id"
    I found out that the formatting for the app id was preventing messages getting sent to pusher on my end.

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const Pusher = require("pusher");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const config = require("./react-pusher/src/config.json");
+const config = require("./src/config.json");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
 const pusher = new Pusher({
   appId: config.app_id,
   key: config.key,
@@ -16,10 +18,12 @@ const pusher = new Pusher({
   cluster: config.cluster,
   encrypted: true,
 });
+
 app.set("PORT", process.env.PORT || 5000);
 
 app.post("/message", (req, res) => {
   const payload = req.body;
+  console.log(payload);
   pusher.trigger("chat", "message", payload);
   res.send(payload);
 });
